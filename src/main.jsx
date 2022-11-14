6c5c0119bc4a16e5a75ec212da13fe8a550f7ac8
sebastianiv21 <seluti98@gmail.com>
2022-11-13 20:07:30 -0500

    feat: migrate from cra to vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './custom.scss'
 import App from './App'
+import './custom.scss'
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
